bcd40ad3c5fbd23eaca21cb1c2f95d6b2aba552a
Lord Erwan <erwan.a.marechal@gmail.com>
2024-12-13 09:07:47 +0100

    Update database password in config.php

--- config.php/config.php
+++ config.php/config.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'smartbike');
 define('DB_USER', 'erwan');
-define('DB_PASS', '');
+define('DB_PASS', 'votre_mot_de_passe');
 
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
